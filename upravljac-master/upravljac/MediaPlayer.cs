2a020071513975ded233b5db58b98a38c8911adb
markogalavic <55134792+markogalavic@users.noreply.github.com>
2020-04-16 16:57:04 +0200

    Add files via upload

---
+++ upravljac-master/upravljac/MediaPlayer.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace upravljac
+{
+    class MediaPlayer
+    {
+        public bool IsStopped;
+
+        public bool IsPaused;
+
+        public bool FastForward;
+
+        public bool FastReverse;
+
+     
+        public int PlayPosition;
+
+
+
+        public MediaPlayer()
+
+        {
+
+           
+        }
+        public bool getIsStopped()
+        {
+            return IsStopped;
+        }
+        public void setIsStopped(bool IsStopped)
+        {
+            this.IsStopped = IsStopped;
+        }
+        public bool getIsPaused()
+        {
+            return IsPaused;
+        }
+        public void setIsPaused(bool IsPaused)
+        {
+            this.IsPaused = IsPaused;
+        }
+        public bool getFastForward()
+        {
+            return FastForward;
+        }
+        public void setFastForward(bool FastForward)
+        {
+            this.FastForward = FastForward;
+        }
+        public bool getFastReverse()
+        {
+            return FastReverse;
+        }
+        public void setFastReverse(bool FastReverse)
+        {
+            this.FastReverse = FastReverse;
+        }
+        public int getPlayPosition()
+        {
+            return PlayPosition;
+        }
+        public void setPlayPosition(int PlayPosition)
+        {
+            this.PlayPosition = 0;
+        }
+
+    }
+}
---
+++ upravljac-master/upravljac/frmPlayer.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace upravljac
+{
+    public partial class frmPlayer : Form
+    {
+        public frmPlayer()
+        {
+            InitializeComponent();
+        }
+
+        private void btnPlay_Click(object sender, EventArgs e)
+        {
+            Console.WriteLine("Započeli ste muziku");
+        }
+
+        private void btnStop_Click(object sender, EventArgs e)
+        {
+            
+            MediaPlayer Stop = new MediaPlayer();
+
+            Stop.setIsStopped(Stop.getIsStopped());
+            Console.WriteLine($"Zaustavili ste medij");
+        }
+
+        private void btnForward_Click(object sender, EventArgs e)
+        {
+            MediaPlayer Fast = new MediaPlayer();
+
+            Fast.setFastForward(Fast.getFastForward());
+            Console.WriteLine($"Ubrzali ste medij");
+
+        }
+
+        private void btnBackward_Click(object sender, EventArgs e)
+        {
+            MediaPlayer Backwards = new MediaPlayer();
+
+            Backwards.setFastReverse(Backwards.getFastReverse());
+            Console.WriteLine($"Ubrzali ste medij");
+
+        }
+
+        private void btnPause_Click(object sender, EventArgs e)
+        {
+            MediaPlayer Pause = new MediaPlayer();
+
+            Pause.setIsPaused(Pause.getIsPaused());
+            Console.WriteLine($"Pauzirali ste medij");
+        }
+    }
+}
